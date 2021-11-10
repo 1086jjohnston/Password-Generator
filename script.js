4339fee78646b1235e22829043a20613694dc625
1086jjohnston <1086jjohnston@gmail.com>
2021-11-10 13:41:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment code here
 function generatePassword() {
-    var length = 8
+    var length = Math.floor(Math.random() * (128-8) + 8)
         charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]\{}|;':<>?,./`~",
         retVal = "";
     for (var i = 0, n = charset.length; i < length; ++i) {
